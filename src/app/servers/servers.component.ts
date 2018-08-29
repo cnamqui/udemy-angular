8beea3a82d3d03581e26836c363f8e0eb692f3ba
bnamqui <baron.namqui>
2018-08-29 15:14:21 +0800

    combining all forms of data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was created';
-  serverName = 'Server Name';
+  serverName: string;
   // classes = 'btn btn-primary';
   // classes = {
   //   btn: true,
