29b09828909cd6064ab0a46cdb8ebafe69707fb5
bnamqui <baron.namqui>
2018-09-01 00:41:15 +0800

    ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,13 +10,16 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = '';
   serverName: string;
   serverCreated = false;
+  servers = ['Server 1', 'Server 2', 'Server 3'];
 
   ngOnInit() {}
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'A Server was created successfully: Name is ' + this.serverName;
+    this.serverName = '';
   }
 
   onUpdateServerName(event: Event) {
