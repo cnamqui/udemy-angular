a1db443c6eb6119cba36ff278d37b96591eb22ec
bnamqui <baron.namqui>
2018-09-01 05:53:26 +0800

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { ServerComponent } from './server/server.component';
-import { ServerAltComponent } from './server-alt/server-alt.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
-    ServerComponent,
-    ServerAltComponent
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
